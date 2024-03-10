236549d7e44279c8cd58edd03cd1e4d035dab289
qianxuege <qianxuege15@gmail.com>
2024-03-10 16:59:18 -0400

    added es7 extension, worked on navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
+// Parts of the code were taken from https://www.youtube.com/watch?v=I2UBjN5ER4s
+
 function App() {
+  const [search, setSearch] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </Router>
+    </>
+
+    // <div className="App">
+    //   <div>
+    //     <input value={search} onChange={(e) => setSearch(e.target.value)}>
+    //     </input>
+    //     <button></button>
+    //   </div>
+    //   </div>
+    
+
+      /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Radixweb</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,8 +41,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+      </header> */
+    
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
+
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true);
+        } 
+    };
+
+    window.addEventListener('resize', showButton);
+    // calls the showButton function whenevern window resizes
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        CMUCal <i class="fa-regular fa-calendar"></i>
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fa-solid fa-x' : 'fa-solid fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick = {closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick = {closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/products' className='nav-links' onClick = {closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick = {closeMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
