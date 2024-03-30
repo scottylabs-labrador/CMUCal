bc9c7f677abaf9c4af20c4873ebb067ed7994fe6
qianxuege <qianxuege15@gmail.com>
2024-03-30 16:22:13 -0400

    added more pages

---
+++ src/components/pages/Careers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Careers() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Careers
---
+++ src/components/pages/Clubs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Clubs() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Clubs
