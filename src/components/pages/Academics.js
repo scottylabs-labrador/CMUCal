ee460ee59542d6ba22854ad9be542db2d9a9bed7
Alice <lehoangchauanh@gmail.com>
2024-04-20 15:16:01 -0400

    Reorganize file locations

--- src/components/pages/Academics.js
+++ src/components/pages/Academics.js
@@ -2,6 +2,26 @@ import React from 'react'
 import { Button } from '../Button'
 import SearchCard from '../SearchCard';
 
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = () => {
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={[]}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 400, width: 800 }}
+      />
+    </div>
+  );
+};
+
 
 function Academics() {
   let eventName1 = "Committee Work Session";
@@ -13,6 +33,7 @@ function Academics() {
     <div>
       <h1>Academics</h1>
       <SearchCard eventName={eventName1} orgName={orgName1} date={date1} time={time1} location={location1} />
+      <MyCalendar />
     </div>
   )
 }
