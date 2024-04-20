b58d627700f1109903fdb283f98325bb4d6073af
Alice <lehoangchauanh@gmail.com>
2024-04-20 16:16:39 -0400

    Use different calendar library

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Button } from '../Button'
+import MyCalendar from '../MyCalendar';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div>
+        <h1>Home</h1>
+      </div>
+      <MyCalendar />
+    </>
   )
 }
 
