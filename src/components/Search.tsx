e9b9789806abab4b963d018f1ec3211e40891677
Alice <lehoangchauanh@gmail.com>
2024-07-01 18:27:16 +0700

    Refactor code. Compact the elements

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -40,6 +40,35 @@ interface SearchComponentProps {
   page: string;
 }
 
+interface SearchBarProps {
+  searchInput: string;
+  setSearchInput: (value: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ searchInput, setSearchInput }) => {
+  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  return (
+    <div className="bg-gray-200 relative h-12 w-full rounded-md border border-black border-[1.5] flex items-center justify-center">
+      <input
+        type="text"
+        placeholder="Search here"
+        onChange={handleChangeSearch}
+        value={searchInput}
+        className="bg-gray-200 flex-grow px-4 focus:outline-none"
+      />
+      {/* Search icon. Add onClick function in the future */}
+      {searchInput ?
+        <RxCross1 onClick={() => setSearchInput("")} className="h-6 w-6 text-gray-500 mr-2" /> :
+        <IoSearch className="h-6 w-6 text-gray-500 mr-2" />
+      }
+    </div>
+  )
+}
+
 const Search: React.FC<SearchComponentProps> = ({ page }) => {
   // Chang name to search bar
   const [searchInput, setSearchInput] = useState("");
@@ -51,8 +80,8 @@ const Search: React.FC<SearchComponentProps> = ({ page }) => {
   const [endDate, setEndDate] = useState<Dayjs>(dayjs().add(7, 'day'));
   const showDatePicker = true;
 
-  
-  useEffect(()=> {
+
+  useEffect(() => {
     // const [data, setData] = useState([]);
 
     // const fuse = new Fuse(SIData, {
@@ -61,14 +90,9 @@ const Search: React.FC<SearchComponentProps> = ({ page }) => {
 
     // setData([fuse.search(searchInput)]);
     getCategoryData(page);
-  },[categoryName])
-  
-  
-  // search
-  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setSearchInput(e.target.value);
-  };
+  }, [categoryName])
+
+
 
   const handleChangeCategory = (event: SelectChangeEvent<typeof categoryName>) => {
     const {
@@ -83,21 +107,21 @@ const Search: React.FC<SearchComponentProps> = ({ page }) => {
   };
 
   const inDateRange = (date: string) => {
-    const startDateObj = startDate.toDate(); 
+    const startDateObj = startDate.toDate();
     const endDateObj = endDate.toDate();
     const eventDate = new Date(date);
     return eventDate >= startDateObj && eventDate <= endDateObj;
   }
 
-  const getCategoryData = (currPage:string) => {
+  const getCategoryData = (currPage: string) => {
     // console.log("running category data");
-    switch(currPage) {
+    switch (currPage) {
       case "academics":
         const optionsAcademics = { keys: ['resource_type', 'course_id', 'course_name'] }
         let combinedData = [...DITData, ...SIData, ...PTData];
         const fuseAcademics = new Fuse(combinedData, optionsAcademics);
         // const fuse = new Fuse(SIData, options);
-        if (categoryName.length == 0 || categoryName.length==4) {
+        if (categoryName.length == 0 || categoryName.length == 4) {
           return fuseAcademics.search(searchInput);
         } else {
           if (!categoryName.includes("Supplemental Instructions")) {
@@ -141,74 +165,74 @@ const Search: React.FC<SearchComponentProps> = ({ page }) => {
       const targetDay = new Date(today);
       targetDay.setDate(today.getDate() - offSet);
       return targetDay.toDateString();
-    }    
+    }
   }
 
   const getNumCategories = () => {
-    if (page==="academics") {
+    if (page === "academics") {
       return categoryListAcademics.length;
-    } else if (page==="clubs") {
+    } else if (page === "clubs") {
       return categoryListClubs.length;
-    } else if (page==="career") {
+    } else if (page === "career") {
       return categoryListCareer.length;
     }
   }
 
   // style for dropdown filter
-	const ITEM_HEIGHT = 48;
-	const ITEM_PADDING_TOP = 8;
-	const MenuProps = {
-	  PaperProps: {
-		style: {
-		  maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-		  width: 450,
-		},
-	  },
-	};
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 450,
+      },
+    },
+  };
 
   const categoryContent = (
     <FormControl className="w-full h-10">
       {/* <InputLabel id="demo-multiple-checkbox-label">Categories</InputLabel> */}
       <Select
-          multiple
-          displayEmpty
-          value={categoryName}
-          onChange={handleChangeCategory}
-          input={<OutlinedInput />}
-          renderValue={(selected) => {
-            if (selected.length === 0) {
-              return <span>Categories</span>;
-            }
-            return selected.join(', ');
-          }}
-          MenuProps={MenuProps}
-          inputProps={{ 'aria-label': 'Without label' }}
-          className="bg-white h-10"
-        >
-        
+        multiple
+        displayEmpty
+        value={categoryName}
+        onChange={handleChangeCategory}
+        input={<OutlinedInput />}
+        renderValue={(selected) => {
+          if (selected.length === 0) {
+            return <span>Categories</span>;
+          }
+          return selected.join(', ');
+        }}
+        MenuProps={MenuProps}
+        inputProps={{ 'aria-label': 'Without label' }}
+        className="bg-white h-10"
+      >
+
         <MenuItem disabled value="">
           <em>Categories ({getNumCategories()})</em>
         </MenuItem>
-       
-          
-          {/* different categories dropdown depending on the page */}
-        {page==="academics" && categoryListAcademics.map((category) => (
+
+
+        {/* different categories dropdown depending on the page */}
+        {page === "academics" && categoryListAcademics.map((category) => (
           <MenuItem key={category} value={category}>
             <Checkbox checked={categoryName.indexOf(category) > -1} />
             <ListItemText primary={category} />
           </MenuItem>
         ))}
-        {page==="clubs" && categoryListClubs.map((category) => (
+        {page === "clubs" && categoryListClubs.map((category) => (
           <MenuItem key={category} value={category}>
             <Checkbox checked={categoryName.indexOf(category) > -1} />
             <ListItemText primary={category} />
           </MenuItem>
         ))}
-        {page==="career" && categoryListCareer.map((category) => (
+        {page === "career" && categoryListCareer.map((category) => (
           <MenuItem key={category} value={category}>
             <Checkbox checked={categoryName.indexOf(category) > -1} />
             <ListItemText primary={category} />
-          </MenuItem>  
+          </MenuItem>
         ))}
       </Select>
     </FormControl>
@@ -228,90 +252,73 @@ const Search: React.FC<SearchComponentProps> = ({ page }) => {
   let dateContent;
   if (showDatePicker) {
     dateContent = (
-      <>      
+      <>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <div className="w-36">
-          <DatePicker
-            value={startDate}
-            onChange={(date) => {
-              startDateFn(date);
-            }}
-            format="MM/DD"
-          />
-        </div>
-        <span className="w-4">to</span>
-        <div className="w-36">
-          <DatePicker 
-            value={endDate}
-            onChange={(date) => {
-              endDateFn(date);
-            }}
-            format="MM/DD"
-          />
-        </div>
+          <div className="w-36">
+            <DatePicker
+              value={startDate}
+              onChange={(date) => {
+                startDateFn(date);
+              }}
+              format="MM/DD"
+            />
+          </div>
+          <span className="w-4">to</span>
+          <div className="w-36">
+            <DatePicker
+              value={endDate}
+              onChange={(date) => {
+                endDateFn(date);
+              }}
+              format="MM/DD"
+            />
+          </div>
         </LocalizationProvider>
       </>
     );
   }
 
-  // bogus values
-  const eventName1 = "Office Hours";
-  const orgName1 = "15-122 Course Staff";
-  const startDate1 = "06/17"
-  const startTime1 = "3PM"
-  const endDate1 = "06/17"
-  const endTime1 = "5PM"
-  const location1 = "POS 146";
-  // const eventCategory1 = "academic";
-  // const eventSubcategory1 = "OfficeHour";
   return (
     <div className="bg-[#F5F5F5] relative -top-2 w-full min-h-screen pl-8 pt-7 text-sans">
-      <div className="flex flex-col w-11/12 gap-y-2.5">
+      <div className="flex flex-col w-11/12 gap-y-2">
+        {/* Scroll bar of saved searches */}
         <div className="no-scroll-bar flex flex-nowrap flex-row gap-x-1.5 overflow-scroll">
-          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs"/>
-          <SavedSearchBtn content="programming" clickStay={true} textSize="text-xs"/>
-          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs"/>
-          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs"/>
-          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs"/>
-          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs"/>
+          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs" />
+          <SavedSearchBtn content="programming" clickStay={true} textSize="text-xs" />
+          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs" />
+          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs" />
+          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs" />
+          <SavedSearchBtn content="15122" clickStay={true} textSize="text-xs" />
         </div>
-        <div className="bg-gray-200 relative h-12 w-full rounded-md border border-black border-[1.5] flex items-center justify-center">
-          <input
-            type="text"
-            placeholder="Search here"
-            onChange={handleChangeSearch}
-            value={searchInput}
-            className="bg-gray-200 flex-grow px-4 focus:outline-none"
-          />
-          {/* Search icon. Add onClick function in the future */}
-          {searchInput? 
-            <RxCross1 onClick = {()=> setSearchInput("")} className="h-6 w-6 text-gray-500 mr-2" /> : 
-            <IoSearch className="h-6 w-6 text-gray-500 mr-2" />
-          }
-          
+        
+        {/* Search Bar */}
+        <div>
+          <SearchBar searchInput={searchInput} setSearchInput={setSearchInput}/>
         </div>
 
-        <div className="mt-3 flex w-full items-baseline justify-between">
+        {/* Time and category row */}
+        <div className="mt-1 flex w-full items-baseline justify-between">
           <div className="w-3/5 items-center flex flex-row justify-between space-x-2">{dateContent}</div>
           {/* drop down filter */}
-          {(page === "academics" || page === "clubs" || page==="career") ? (
+          {(page === "academics" || page === "clubs" || page === "career") ? (
             <div className="w-2/6">{categoryContent}</div>
           ) : (
             <></>
           )}
         </div>
-        {/* <div className="w-full mt-2">{actionsMenuComp}</div> */}
-        <div className="w-full mt-2 flex justify-between">
-          {/* <Dropdown/> */}
-          <Button content="Add All" clickStay={false} textSize="text-base"/>
-          <Button content="Reset CMUCal Events" clickStay={false} textSize="text-base"/>
+
+        {/* Add all & Reset row */}
+        <div className="w-full mt-1 flex justify-between text-base">
+          <div className="py-1 px-4 rounded-3xl border border-teal">Add All</div>
+          <div className="py-1 px-4 rounded-3xl border border-teal">Reset CMUCal Events</div>
         </div>
-        
-        <div className="overflow-scroll" style={{height: '70vh'}}>
-        {searchInput && page==="academics" && getCategoryData(page)?.map( (result) => {
-          for (let i = 0; i<result.item.events.length; i++) {
-            if (inDateRange(getWeekday(result.item.events[i].weekday))) {
-              return ( 
+
+        {/* Mapped search cards */}
+        <div className="overflow-scroll" style={{ height: '70vh' }}>
+          {searchInput && page === "academics" && getCategoryData(page)?.map((result) => {
+            for (let i = 0; i < result.item.events.length; i++) {
+              if (inDateRange(getWeekday(result.item.events[i].weekday))) {
+                return (
                   <SearchCard
                     key={result.refIndex}
                     eventName={`${result.item.resource_type} for ${result.item.course_name}`}
@@ -322,14 +329,14 @@ const Search: React.FC<SearchComponentProps> = ({ page }) => {
                     endTime={result.item.events[i].end_time}
                     location={result.item.events[i].location}
                   />
-              )
-            }  
-          }
-        })}
-        {searchInput && (page==="clubs" || page==="career") && getCategoryData(page)?.map( (result) => {
-          for (let i = 0; i<result.item.events.length; i++) {
-            if (inDateRange(getWeekday(result.item.events[i].weekday))) {
-              return ( 
+                )
+              }
+            }
+          })}
+          {searchInput && (page === "clubs" || page === "career") && getCategoryData(page)?.map((result) => {
+            for (let i = 0; i < result.item.events.length; i++) {
+              if (inDateRange(getWeekday(result.item.events[i].weekday))) {
+                return (
                   <SearchCard
                     key={result.refIndex}
                     eventName={`${result.item.event_name}`}
@@ -340,17 +347,17 @@ const Search: React.FC<SearchComponentProps> = ({ page }) => {
                     endTime={result.item.events[i].end_time}
                     location={result.item.events[i].location}
                   />
-              )
-            }  
-          }
-        })}
+                )
+              }
+            }
+          })}
         </div>
 
 
-        
+
       </div>
     </div>
   );
 }
 
-export {Search};
+export { Search };
